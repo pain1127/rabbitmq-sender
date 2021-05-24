3403f7dc3bcb3f471eafed15aa929c07e29b1caf
pain1127 <sk.park@catenoid.net>
2021-05-25 01:04:08 +0900

    커밋

--- src/config/common.js
+++ src/config/common.js
@@ -6,6 +6,9 @@ dotenv.config();
 const config = {
   env: process.env.NODE_ENV,
   port: process.env.USE_PORT,
+  rabbitmq: {
+    url: process.env.RABBITMQ_URL,
+  },
 };
 
 module.exports = config;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,49 @@
+/* eslint-disable require-jsdoc */
 const express = require('express');
 const router = new express.Router();
-
+const amqp = require('amqplib/callback_api');
+const config = require('../config/common');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Express'});
-});
+router.get('/', async (req, res, next) => {
+  // amqp://admin:admin@localhost admin:admin = rabbitmq 계정:암호
+  amqp.connect('amqp://pain1127:psk2950@localhost', function(error0, connection) {
+    
+    if (error0) {
+      console.log(error0);
+    }
 
+    connection.createChannel(function(error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+
+      // queue name
+      const queue = 'test';
+
+      /*
+      * queue가 없으면 만들어줌
+      * durable : true -> queue 데이터를  rabbitmq가 재시작해도 가지고 있음(소비하기전까지)
+      */
+      channel.assertQueue(queue, {
+        durable: true,
+      });
+      setInterval(sendToQueue, 1000, channel, queue);
+    });
+
+    setTimeout(function() {
+      connection.close();
+      process.exit(0);
+    }, 50000);
+  });
+
+
+  // const connect = await amqp.connect(config.rabbitmq.url);
+  // const channel = await connect.createChannel();
+  // res.send(channel);
+});
+function sendToQueue(channel, queue) {
+  const msg = 'Hello World! transDate:' + new Date();
+  channel.sendToQueue(queue, Buffer.from(msg));
+  console.log(' [x] Sent %s', msg);
+}
 module.exports = router;
